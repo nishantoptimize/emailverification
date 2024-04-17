4c81d93238aa1fe1e72d93f6dd50f3446d0fd57e
Nishant <nishant@gmil.com>
2024-04-17 16:32:18 +0530

    commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+    $post_title = "Home page";
+    include('includes\header.php');
+    include('includes\navbar.php'); 
+?>
+
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2>Login and registratin with php</h2>
+                <h4>with email verification</h4>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes\foter.php') ?>
